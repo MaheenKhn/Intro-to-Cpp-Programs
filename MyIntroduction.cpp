29e79eea3f54810fc0a658f842b28106afdbb595
MaheenKhn <maheenbilalkhan67@gmail.com>
2025-11-25 06:22:15 -0800

    Upload MyIntroduction.cpp
    This file prints my basic introduction using cout statements.
     First C++ practice program of my first semester.

---
+++ MyIntroduction.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    cout << "My name is Maheen Bilal." << endl;
+    cout << "I am a BS Software Engineering student." << endl;
+    cout << "I am studying in UETM." << endl;
+    cout << "I am learning basics of C++." << endl;
+    return 0;
+
+    }
